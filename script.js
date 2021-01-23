ea5167bf8700b13e730591529dd602a8eedace47
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-01-23 09:14:18 +0300

    this is the point where I'M stuck bad, so it's a FALLBACK

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary= [{}];
+//let myLibrary= [{bookName:"der steppenwolf", author: "hermann hesse"}, {bookName: "underground", author:"dostoyevski"}];
 
 function Book(bookName, author) {
     // the constructor...
@@ -7,7 +7,7 @@ function Book(bookName, author) {
   }
   
   function addBookToLibrary() {
-    // get the user input 
+// get the user input 
 let getBooks= document.getElementById("bookName").value;
 let getAuthor= document.getElementById("author").value;
 //create object(s) from Book prototype
@@ -21,20 +21,24 @@ myLibrary.push(bookInput);
 document.getElementById("bookName").value=" ";
 document.getElementById("author").value=" ";
   }
-      //get the table 
-      let table= document.getElementById("tableList");  
-let texto= document.create
+   
+  
+
+
+
   function listBooks(){
+    let myLibrary= [{bookName:"der steppenwolf", author: "hermann hesse"}, {bookName: "underground", author:"dostoyevski"}];
 
 
-    for (let i =1; i<myLibrary.length; i++) 
+    for (let i =0; i<myLibrary.length; i++) 
     {
       // console.log(`row ${i}`);
-       for (const j in myLibrary[i]) 
+       for (let keys in myLibrary[i]) 
          {
           //console.log(` ${myLibrary[i][j]}`);
-            let x = myLibrary[i][j];
+            let x = myLibrary[i][keys];
             console.log(x);
+            //x is string
           
            
         }

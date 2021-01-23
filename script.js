a758f1ce2d29f2fb74e4d95ad57c298bb3e2469f
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-01-23 05:02:08 +0300

    added the object creation, and also once it's submitted, the input field is replenished

--- script.js
+++ script.js
@@ -7,5 +7,22 @@ function Book(bookName, author) {
   }
   
   function addBookToLibrary() {
-    // do stuff here
+    // get the user input 
+let getBooks= document.getElementById("bookName").value;
+let getAuthor= document.getElementById("author").value;
+console.log(getBooks,getAuthor);
+//create object(s) from Book prototype
+let bookInput= Object.create(Book);
+//the user input will be object's values
+bookInput.bookName= getBooks;
+bookInput.author= getAuthor;
+//console log to check performance
+console.log(bookInput.bookName);
+console.log(bookInput.author);
+console.log(bookInput);
+//empty the user input field
+document.getElementById("bookName").value=" ";
+document.getElementById("author").value=" ";
+
+
   }

22b9b5082dde21aa5e5d2c91b32eae286eb81e2d
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-01-23 04:09:03 +0300

    added the object constructor function

---
+++ script.js
@@ -0,0 +1,11 @@
+let myLibrary= [];
+
+function Book(bookName, author) {
+    // the constructor...
+    this.bookName= bookName,
+    this.author= author
+  }
+  
+  function addBookToLibrary() {
+    // do stuff here
+  }

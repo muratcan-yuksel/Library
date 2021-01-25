50b2a870542f158bf65fa124588c424709f56084
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-01-25 06:09:13 +0300

    added the read/not read dropdown select item

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 //Array that scores all the books
 let myLibrary= [{bookName:"der steppenwolf", author: "hermann hesse"}, {bookName: "underground", author:"dostoyevski"}];
 //The object constructor
-function Book(bookName, author) {
-    this.bookName= bookName,
-    this.author= author
+    function Book(bookName, author) {
+        this.bookName= bookName,
+        this.author= author
   }
   //The function that adds books to the myLibrary array
   function addBookToLibrary() {
-// get the user input 
-let getBooks= document.getElementById("bookName").value;
-let getAuthor= document.getElementById("author").value;
-//create object(s) from Book prototype
-let bookInput= Object.create(Book);
-//the user input will be object's values
-bookInput.bookName= getBooks;
-bookInput.author= getAuthor;
-//push the objects into the array
-myLibrary.push(bookInput);
-//console log to check the library
-console.log(myLibrary);
-//a functin that takes the user input and turns it into a table
-function displayBook(){
-     //create table
-  var table = document.createElement("table");
-      document.body.appendChild(table);
-    // var table = document.getElementById("myTable");
-  var row = table.insertRow(0);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-      cell1.textContent = getBooks;
-      cell2.innerHTML = getAuthor;
+        // get the user input 
+        let getBooks= document.getElementById("bookName").value;
+        let getAuthor= document.getElementById("author").value;
+        //create object(s) from Book prototype
+        let bookInput= Object.create(Book);
+        //the user input will be object's values
+        bookInput.bookName= getBooks;
+        bookInput.author= getAuthor;
+        //push the objects into the array
+        myLibrary.push(bookInput);
+        //console log to check the library
+        console.log(myLibrary);
+        //a functin that takes the user input and turns it into a table
+  function displayBook(){
+          //create table
+        var table = document.createElement("table");
+            document.body.appendChild(table);
+          // var table = document.getElementById("myTable");
+        var row = table.insertRow(0);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+            cell1.textContent = getBooks;
+            cell2.innerHTML = getAuthor;
 };
 //call the book adding function
 displayBook();
@@ -39,25 +39,21 @@ document.getElementById("bookName").value=" ";
 document.getElementById("author").value=" ";
   }
 
-
-
- 
-
-//loop through the array
-function loopArr(){
-  myLibrary.forEach((element,index)=>{
-      console.log(element.bookName); // 100, 200, 300
-      console.log(element.author);
-      console.log(index); // 0, 1, 2
-      //create table
-      var table = document.createElement("table");
-      document.body.appendChild(table);
-      // var table = document.getElementById("myTable");
-       var row = table.insertRow(0);
-       var cell1 = row.insertCell(0);
-       var cell2 = row.insertCell(1);
-       cell1.textContent = element.bookName;
-       cell2.innerHTML = element.author;
-     
+//loop through the array and display the books as table
+  function loopArr(){
+          myLibrary.forEach((element,index)=>{
+            console.log(element.bookName); // 100, 200, 300
+            console.log(element.author);
+            console.log(index); // 0, 1, 2
+            //create table
+            var table = document.createElement("table");
+            document.body.appendChild(table);
+            // var table = document.getElementById("myTable");
+            var row = table.insertRow(0);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            cell1.textContent = element.bookName;
+            cell2.innerHTML = element.author;
+          
   });
   }

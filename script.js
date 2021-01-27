8cd5c2c6068a211274397bfebef9627b3aeba3ae
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-01-27 11:00:01 +0300

    FALLBACK, CORE FUNCTIONALTY works perfectly. Now I need to style it and use firebase

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Array that scores all the books
-let myLibrary= [{bookName:"der steppenwolf", author: "hermann hesse"}, {bookName: "underground", author:"dostoyevski"}];
+let myLibrary= [{bookName:"Der steppenwolf", author: "Hermann Hesse"}, {bookName: "Notes from the Underground", author:"Fyodor Mikhailovich Dostoevsky"}];
 //The object constructor
     function Book(bookName, author) {
         this.bookName= bookName,
@@ -81,7 +81,7 @@ function deleteButton () {
                 this.parentNode.parentNode.removeChild(this.parentNode);
                 //find the index that contains the book name and author related to the delete button
               index= myLibrary.findIndex(obj=> obj.bookName === getBooks && obj.author === getAuthor);
-                //remove tat index
+                //remove that index
               myLibrary.splice(index,1);
               }
 
@@ -110,9 +110,51 @@ deleteButton();
             var cell2 = row.insertCell(1);
             cell1.textContent = element.bookName;
             cell2.innerHTML = element.author;
-            let cell3=row.insertCell(2);
-            //A button needs to be added here 
-            cell3.textContent="deneme";
+                // creating button element  for the read/NotRead status
+                let button = document.createElement('BUTTON');     
+                //create a class for these buttons
+                button.className="statusButton";   
+                button.textContent= "Read";  
+                table.appendChild(button); 
+
+                  //set attribute for swapping texts on click
+              button.setAttribute("data-text-swap", "Not Read");
+              button.setAttribute("data-text-original", "Read");
+                //function that changes the books read/Notread status on click
+                button.addEventListener('click', function() {
+                  if (button.getAttribute("data-text-swap") == button.innerHTML) {
+                    button.innerHTML = button.getAttribute("data-text-original");
+                  } else {
+                    button.setAttribute("data-text-original", button.innerHTML);
+                    button.innerHTML = button.getAttribute("data-text-swap");
+                  }
+                }, false);
+
+                function deleteButton () {
+                  //create remove button
+                        var removeButton = document.createElement('BUTTON');   
+                  // creating text to be displayed on button 
+                        var removeButtonText = document.createTextNode("Delete"); 
+                        //append the text to the button
+                        removeButton.appendChild(removeButtonText);
+                        //append remove button to table
+                        table.appendChild(removeButton);
+                          //add an event listener to the delete button
+                        removeButton.addEventListener("click", initRemoveButton, false);
+                        //delete its parent element 
+                        function initRemoveButton(){
+                          this.parentNode.parentNode.removeChild(this.parentNode);
+                          //find the index that contains the book name and author related to the delete button
+                          //the difference here from the above function is elemen.bookName or author
+                        index= myLibrary.findIndex(obj=> obj.bookName === element.bookName && obj.author === element.author);
+                          //remove that index
+                        myLibrary.splice(index,1);
+
+                        }
+          
+             };
+          //call delete button function
+          deleteButton();
           
   });
   }
